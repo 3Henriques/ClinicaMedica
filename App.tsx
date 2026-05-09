05d856297ea7598eaf0cf4204d1afe47b23c7cdd
carvalh0Henrique <carvalhoghenrique@gmail.com>
2026-05-09 00:23:57 -0300

    feat(): Implementa o touchablewithoutfeedback em telas globais

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Keyboard, TouchableWithoutFeedback } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { AppNavigator } from "./src/navigation/AppNavigator";
@@ -7,11 +8,13 @@ import { Cores } from "./src/styles/Tema";
 
 export default function App() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <AuthProvider>
-        <StatusBar style="light" backgroundColor={Cores.fundoPrimario} />
-        <AppNavigator />
-      </AuthProvider>
-    </GestureHandlerRootView>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <GestureHandlerRootView style={{ flex: 1 }}>      
+        <AuthProvider>
+          <StatusBar style="light" backgroundColor={Cores.fundoPrimario} />
+          <AppNavigator />
+        </AuthProvider>      
+      </GestureHandlerRootView>
+    </TouchableWithoutFeedback>
   );
 }
