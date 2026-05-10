30e4a6a0454eef27abd20fe8f48c4b6d80bf59a3
Henrique A. R. <henrique4r.it@gmail.com>
2026-05-10 15:42:40 -0300

    refactor(models): alinha medico ao código atualizado

--- src/models/Medico.ts
+++ src/models/Medico.ts
@@ -1,9 +1,15 @@
 import { DiasAtendimento } from "./DiasAtendimento";
+import { Endereco } from "./valueObjects/Endereco";
+import { Sexo } from "./enums/Sexo";
 
 export interface Medico {
   matricula: number;
   crm: string;
   nome: string;
+  sexo: Sexo;
+  dtNascimento: string;
+  endereco: Endereco;
+  telefones: string[];
   especialidades: number[];
   diasAtendimento: DiasAtendimento[];
 }
