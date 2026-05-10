c2f65a17d3e3104f204bf65eb10fb41ea3e17f51
Henrique A. R. <henrique4r.it@gmail.com>
2026-05-10 15:43:30 -0300

    refactor(models): alinha paciente ao código atualizado

--- src/models/Cliente.ts
+++ src/models/Cliente.ts
@@ -1,13 +1,16 @@
+import { Endereco } from "./valueObjects/Endereco";
+import { Sexo } from "./enums/Sexo";
+
 export interface Cliente {
   identificador: number;
   nome: string;
   dtNascimento: string;
   email: string;
-  telefone: string;
-  logradouro: string;
-  numero: number;
-  complemento?: string;
-  bairro: string;
-  cidade: string;
-  estado: string;
+  telefone: string[];
+  sexo: Sexo;
+  RG: string;
+  CPF: string;
+  endereco: Endereco;
+  nomeCovenio: string;
+  matriculaConveniado: string;
 }
