44e6f5ae6ed161eaaa470c1c673a2fefc0cdbd73
Henrique A. R. <henrique4r.it@gmail.com>
2026-05-10 13:54:58 -0300

    feat(mocks): adiciona um atendimento realizado do tipo "retorno" para apresentar consultas sem cobrança

--- src/mocks/consultas.ts
+++ src/mocks/consultas.ts
@@ -13,6 +13,7 @@ export const consultasMock: Consulta[] = [
   { numero: 4, clienteId: 4, medicoId: 3, especialidadeId: 4, data: fmt(ontem), horaInicio: "11:00", horaFim: "11:30", tipo: "NOVA", status: "E", valor: 250, tipoPagamento: "PIX", procedimentos: "Avaliacao" },
   { numero: 5, clienteId: 5, medicoId: 2, especialidadeId: 3, data: fmt(hoje), horaInicio: "12:00", horaFim: "12:30", tipo: "NOVA", status: "C", motivoCancelamento: "SOLICITACAO_CLIENTE" },
   { numero: 6, clienteId: 6, medicoId: 3, especialidadeId: 5, data: fmt(hoje), horaInicio: "13:00", horaFim: "13:30", tipo: "NOVA", status: "X", motivoCancelamento: "SOLICITACAO_MEDICO" },
+  { numero: 9, clienteId: 6, medicoId: 2, especialidadeId: 2, data: fmt(hoje), horaInicio: "16:00", horaFim: "16:30", tipo: "RETORNO", status: "R", tipoPagamento: "DINHEIRO", procedimentos: "Acompanhamento" },
   { numero: 7, clienteId: 1, medicoId: 1, especialidadeId: 2, data: fmt(hoje), horaInicio: "14:00", horaFim: "14:30", tipo: "RETORNO", status: "M" },
   { numero: 8, clienteId: 2, medicoId: 1, especialidadeId: 2, data: fmt(ontem), horaInicio: "15:00", horaFim: "15:30", tipo: "RETORNO", status: "E", valor: 0, tipoPagamento: "CONVENIO" },
 ];
